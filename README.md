93a038820ee127f1ac6b98a8122660b61be08860
jun-canbuild <wdai2@gmail.com>
2018-12-19 00:01:45 -0800

    Final Code

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # python_final
+#
+# Both script scraps data from https://www.sacredlotus.com/ that are necessary to create the visualization for the final project and output data in a .json file
+
+# The first script is formula.py, which collects the URLs of all 257 formula provided on this page https://www.sacredlotus.com/go/chinese-formulas/category/all
+
+# Then I went into formula.json and delete the last dictionary that contains links from amazon
+
+# The second script is herbs.py, which scraps all the herbs exist in each formula and the quantity required by visiting the URLs found in the first script formula.py
--- herbs.py
+++ herbs.py
@@ -1,20 +1,15 @@
 from bs4 import BeautifulSoup
-import requests, json, time
+import requests, json, time, re
+
+all_data = []
+each_formula_herb = []
+
+
+time.sleep(0.25)
 
 with open ('title.json') as formula_titles:
-    #text = json.loads(formula_titles)
     loadData = json.load(formula_titles)
-    # print ('-------')
-    # print ('first object\'s formulas')
-    # print (loadData[0]['formulas'][0]['formula_link'])
-    # print ('-------')
-    #
-    # print ('------')
-    # print (loadData[1]['formulas'][0]['formula_link'])
-    # print ('------')
-    # print (loadData[1]['formulas'][1]['formula_link'])
-    # print ('------')
-    # print (loadData[1]['formulas'][2]['formula_link'])
+
 
 
     for all in loadData:
@@ -22,58 +17,50 @@ with open ('title.json') as formula_titles:
 
         for urlextension in formulas:
             formula_link = urlextension['formula_link']
-            final_url = "https://www.sacredlotus.com" + formula_link
+            final_url = 'https://www.sacredlotus.com' + formula_link
+            print('scrapping url:' + final_url);
 
             herb_page = requests.get(final_url)
 
             page_html = herb_page.text
 
-            soup = BeautifulSoup(page_html, "html.parser")
+            soup = BeautifulSoup(page_html, 'html.parser')
 
-            all_herbs = soup.find_all("div", attrs={"class":"tcm-content"})
-
-            #all_name = soup.find_all(string = "Quantity")
-
-            time.sleep(0.25)
-
-            print(all_herbs)
-            # for a_herb in all_herbs:
-            #
-            #     the_herb = a_herb.text
-            #
-            #     print(the_herb)
+            all_substance_links = soup.find_all('a', href = re.compile(r'/go/chinese-herbs/substance/.*'))
 
 
+            for link in all_substance_links:
 
 
+                substance = {}
+                substance['name'] = link.text
+                substance['link'] = link['href']
+                substance['name_english'] = link.next_sibling
+                substance['quantity'] = link.next_sibling.next_sibling.next_sibling
+
+                if substance['quantity'] == None:
+                    continue
+                else:
+                    substance['quantity'] = substance['quantity'].replace('Quantity = ','')
+                    substance['quantity'] = substance['quantity'].replace('grams','')
+                    substance['quantity'] = substance['quantity'].strip()
+                    substance['quantity'] = substance['quantity'].split("-")[0]
+
+                row_div = link.next_sibling.next_sibling.next_sibling.next_sibling
+
+                herb_details = row_div.find_all("div", {'class':'herb-detail clearfix'})
+
+                for detail in herb_details:
 
 
+                    key = detail.find('div',{'class':"herb-detail-left"}).text
 
+                    key = key.replace(' ','_').replace(':','').lower()
 
+                    value = detail.find('div',{'class':"herb-detail-right"}).text
 
+                    substance['key'] = value
 
-    # print (loadData[0][0])
-#     for url in formula_titles:
-#         print(url)
-#
-#         x = json.loads(url)
-#         print(x)
+                all_data.append(substance)
 
-# x = json.load(title)
-# print(x)
-
-# all_data = []
-# quantity_each = []
-# channel = []
-# property = []
-# time.sleep(2)
-#
-# formula = json.load(formula.json)
-#
-# for url in formula['formula_link']:
-#     url = "https://www.sacredlotus.com + "
-#     print("Scraping!",url)
-#     herb_page = requests.get(url)
-#
-#     page_html = herb_page.text
-#     soup = BeautifulSoup(page_html, "html.parser")
+json.dump(all_data,open('herbs.json', 'w'),indent=2)
